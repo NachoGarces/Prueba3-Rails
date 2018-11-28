c392ae5d79495dc8d3cfdb7416c11cc57e978ef2
ignacio <nachogarces15@gmail.com>
2018-11-28 20:26:41 -0300

    Create and Show

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,18 @@ class TodosController < ApplicationController
   def new
     @todo = Todo.new
   end
+
+  def create
+    @todo = Todo.new(todo_params)
+    redirect_to todos_path if @todo.save
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
 end
