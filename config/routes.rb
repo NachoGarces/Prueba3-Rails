ce847da1c4597205fd191496e9180d194e6c44f4
ignacio <nachogarces15@gmail.com>
2018-11-29 23:59:36 -0300

    Error correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get '/todos/list', as: 'list_todo'
-  resources :todos
+  get '/todos', to: 'todos#index'
+  get 'todos/new', as: 'new_todo'
+  post '/todos', to: 'todos#create'
+  get '/todos/:id', to: 'todos#show', as: 'todo'
+  get '/todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
+  patch '/todos/:id', to: 'todos#update'
+  delete '/todos/:id', to: 'todos#destroy'
   get '/todos/:id/complete', to: 'todos#complete', as: 'complete_todo'
   root 'todos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
